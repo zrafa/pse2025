e29d78b40156b3c8dc8887a4abefd41109395152
Antonella Torr <bantonellatorres@gmail.com>
2025-05-02 23:09:40 -0300

    Ejercicio pianito listo y game en curso

---
+++ antonella.torres/lab3/game/serial.h
@@ -0,0 +1,20 @@
+/**********************************************************************
+ *
+ * Filename:    serial.h
+ * 
+ * API para la aplicacion embebida 
+ * META : ocultar el hardware a la aplicacion 
+ *
+ **********************************************************************/
+
+#ifndef _SERIAL_H
+#define _SERIAL_H
+
+
+void serial_init(void);
+void serial_put_char(char c);
+char serial_get_char(void);
+void serial_put_escape_sequence(const char* seq);
+
+
+#endif /* _SERIAL_H */
