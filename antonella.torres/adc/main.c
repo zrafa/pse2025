f87ce3e85c618a03064e0f69fa1e7f46d9708577
Antonella Torr <bantonellatorres@gmail.com>
2025-05-18 18:05:30 -0300

    Ejercicio speaker y knob readyy

--- antonella.torres/adc/main.c
+++ antonella.torres/adc/main.c
@@ -1,25 +1,36 @@
-
 #include "adc.h"
 #include "serial.h"
+#include "makesound.h"
 #include <util/delay.h>
 
-main()
+#define MIN_FREQ 800
+#define MAX_FREQ 2800
+
+int main(void)
 {
-	int val;
-
-	adc_init();
-	serial_init();
-
+    adc_init();
+    serial_init();
+    init_ultrasound();
 
 
-	while (1) {
-		/* obtener una conversión ADC desde el pin de entrada ADC 2 */
- 		uint16_t val = adc_get(2);
-		serial_put_number(val); 
-		/* realizar alguna acción con val */
+    while (1) {
+        uint16_t val = adc_get(2);
+        serial_put_number(val);
 
-		_delay_ms(500);
-	}
+        uint8_t nivel = val / 64;  // nivel va de 0 a 15
 
-	for(;;);
+     
+        int frec = MIN_FREQ + (nivel * (MAX_FREQ - MIN_FREQ)) / 15;
+
+      
+      
+		serial_put_char('F');
+		serial_put_number(frec);
+		sound(frec, 800);
+        
+
+        _delay_ms(200);
+    }
+
+    return 0;
 }
---
+++ antonella.torres/adc/makesound.h
@@ -0,0 +1,12 @@
+/*
+ * ultrasound: controla el sensor HC-SR04 para medir distancias en cm
+ */
+
+/* ultrasound_get_distance()
+ * 	devuelve la distancia en cm del objeto delante del sensor
+ * 	devuelve -1 si no existe ningun objeto
+ */
+int ultrasound_get_distance(void);
+void init_ultrasound(void);
+void soundT(void);
+void sound(int, int);
